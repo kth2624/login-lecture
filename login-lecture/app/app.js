5b9886696eb569467bd9b18e9ddc35c0e98daf8d
TAEHO KIM <39645477+kth2624@users.noreply.github.com>
2021-06-01 01:11:59 +0900

    폴더구조최적화

--- login-lecture/app/app.js
+++ login-lecture/app/app.js
@@ -6,10 +6,10 @@ const app = express();
 
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //앱셋팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 
